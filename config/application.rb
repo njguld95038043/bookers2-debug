29aa228c279b117842e505d20ec0416718f31905
njguld95038043 <t.tamura6060@gmail.com>
2022-02-05 02:46:08 +0000

    [update]

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module Bookers2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.action_view.form_with_generates_remote_forms = true
   end
 end
