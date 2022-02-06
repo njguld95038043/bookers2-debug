540b3e126481560f747d055fd1a3a4a42d1482a3
njguld95038043 <t.tamura6060@gmail.com>
2022-02-06 06:01:35 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'chat/:id', to: 'chats#show', as: 'chat'
+  resources :chats, only: [:create]
   get 'relationships/followings'
   get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
