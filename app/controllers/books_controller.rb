20c0b1098070d24567967bbfb6a201aeb44a5a3c
njguld95038043 <t.tamura6060@gmail.com>
2022-02-06 04:26:55 +0000

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,12 @@ class BooksController < ApplicationController
     @user = current_user
     @book = Book.new
     @books = Book.all
+    to  = Time.current.at_beginning_of_day
+    from  = (to - 6.day).at_end_of_day
+    @books = Book.all.sort {|a,b|
+      b.favorites.where(created_at: from...to).size <=>
+      a.favorites.where(created_at: from...to).size
+    }
   end
 
   def create
@@ -48,7 +54,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body, :rate)
   end
 
   def ensure_correct_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_012418) do
+ActiveRecord::Schema.define(version: 2022_02_06_011148) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
